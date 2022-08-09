336cad64abd2573247c20a74f9583eb22c9a9afe
SAULvaRGAS88 <saulvargascosta@hotmail.com>
2022-08-09 15:02:18 -0300

    ex01 Refatorado

---
+++ Aula01/ex01 08Ago Refatorado.py
@@ -0,0 +1,16 @@
+
+
+NomeProduto  = ["cafe", "sal", "laranja"]
+
+def imprimir(NomeProduto):
+    print(NomeProduto)
+
+imprimir(NomeProduto[0])
+
+
+
+def removerItem(NomeProduto):
+    print(NomeProduto)
+NomeProduto.remove(NomeProduto[2])
+
+removerItem(NomeProduto)
