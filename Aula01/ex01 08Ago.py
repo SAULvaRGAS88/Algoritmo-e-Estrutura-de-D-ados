f0fc9abc43d6555e5db80e12e9f3300be5226e9d
SAULvaRGAS88 <saulvargascosta@hotmail.com>
2022-08-09 00:11:29 -0300

    Exercicio python 08Ago

---
+++ Aula01/ex01 08Ago.py
@@ -0,0 +1,26 @@
+"""Construir um algoritmo que contenha 3 listas, cada lista contendo:
+• Nomes de produtos
+• Preços de cada produto
+• Quantidades de cada produto
+• Construir uma função para imprimir um dos produtos da lista e uma
+função para retirar um dos produtos das listas. As funções devem receber
+um parâmetro que será usado para acessar a posição dos itens das listas
+que serão impressos ou retirados."""
+
+NomeProduto = ["cafe", "sal", "laranja"]
+PrecoProduto = [5.50, 2, 4]
+QuantidadeProduto =[10, 50, 15] 
+
+def imprimirNome(NomeProduto):
+    return(NomeProduto)
+
+def imprimirProduto(PrecoProduto):
+    return(PrecoProduto)
+
+def imprimirQuantidade(QuantidadeProduto):
+    return(QuantidadeProduto)
+
+print(f'O produto {NomeProduto[0]} custa {PrecoProduto[0]} e tem {QuantidadeProduto[0]} unidades')  
+
+
+def removerItem ([NomeProduto] , [PrecoProduto])
