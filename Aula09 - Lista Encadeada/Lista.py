1587a1491da1465830e4907ed9982520e07cf946
SAULvaRGAS88 <saulvargascosta@hotmail.com>
2022-10-04 19:45:35 -0300

    aula 09

---
+++ Aula09 - Lista Encadeada/Lista.py
@@ -0,0 +1,60 @@
+from tkinter import N
+from No import No
+
+class Lista:
+
+    def __init__(self):
+        self.inicio= None
+
+    def adicionarNoFim(self, valor):
+        nodo = No(valor)
+        if self.inicio == None:
+            self.inicio = nodo
+        else:
+            aux = self.inicio
+            while (aux.proximo):
+                aux = aux.proximo
+            aux.proximo = nodo
+
+    def adicionarNoInicio(self, valor):
+        nodo = No(valor)
+        if self.inicio == None:
+            self.inicio = nodo
+        else:
+            nodo.proximo = self.inicio
+            self.inicio = nodo
+
+    def imprimir(self):
+        if self.inicio == None:
+            print("Lista vazia!\n-------")
+        else:
+            print("-------------\n")
+            aux = self.inicio
+            while(aux):
+                print(aux.dado , "\n")
+                aux = aux.proximo
+
+    def removerDoinicio(self):
+        if self.inicio == None:
+            print("Lista vazia!\n-------")
+        elif self.inicio.proximo == None:
+            self.inicio = None
+        else:
+            self.inicio = self.inicio.proximo
+        self.imprimir()
+
+    def removerDoFim(self):
+        if self.inicio == None:
+            print("Lista vazia!\n-------")
+        elif self.inicio.proximo == None:
+            self.inicio = None
+        else:
+            ant = self.inicio
+            aux = self.inicio.proximo
+            while(aux.proximo):
+                ant = aux
+                aux = aux.proximo
+            ant.proximo = None
+
+        self.imprimir()
+        
---
+++ Aula09 - Lista Encadeada/main.py
@@ -0,0 +1,10 @@
+from Lista import Lista
+
+l1 = Lista()
+
+l1.imprimir()
+l1.adicionarNoInicio('Joao')
+l1.adicionarNoInicio('Maria')
+l1.adicionarNoFim("jose")
+l1.removerDoinicio()
+l1.removerDoFim()
